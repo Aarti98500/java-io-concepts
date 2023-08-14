d4a6df826b6569057f0ae106ac6461613892a16c
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-08-14 22:02:31 +0530

    file Input Output Stream Example

---
+++ byte-streaming/src/BufferedInputOutputStreamExample.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+//resource means objects.
+//try with resource means create objects inside () of try and close them automatically after their use.
+public class BufferedInputOutputStreamExample {
+    public static void main(String[] args) throws IOException {
+        //Read from input file.
+        //below is try with resources example.
+        try (FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Dell\\IdeaProjects\\java-io-concepts\\byte-streaming\\src\\input.txt");
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream) {
+
+//            byte[] bytes = bufferedInputStream.readAllBytes();
+//            System.out.println(new String(bytes));
+
+            //write to output file
+            try (FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Dell\\IdeaProjects\\java-io-concepts\\byte-streaming\\src\\output.txt");
+                 BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);{
+
+                  bufferedOutputStream.write(bufferedInputStream.readAllBytes());
+            }
+
+        }
+        ) }
+}
+
+
+
+
+
--- byte-streaming/src/ObjectInputOutputStreamExample.java
+++ byte-streaming/src/ObjectInputOutputStreamExample.java
@@ -1,2 +1,74 @@
-public class ObjectInputOutputStreamExample {
+import java.io.*;
+
+/**
+ * Working of objectInputStream and objectoutputStream
+ * 1. The objectInputStream is mainly used to read data/object written by the objectOutputStream.
+ * 2. Basically, the objectOutputStream converts or writes java objects into corresponding streams(Outpur stream).
+ *    Those converted streams can be stored in files or transferred through networks.
+ *    This is known as 'Sterilization'.
+ * 3. Now, if we need to read those objects, we will use the objectStream that will
+ *    convert the streams back to corresponding objects. This is known as 'Deserilization'
+ */
+
+class Student implements Serializable {
+    String name;
+    int id;
+
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
+
+//Stream is nothing but seq of elements or flow of data or pipe of data.
+//FileOutputStream -> writes into a file.
+//ObjectOutputStream -> writes to object output stream.
+
+//Input and Output that means Read and Write.
+//Input -> read data
+//Output -> write data
+
+/*
+  The goal of this program is to write the object using objectOutputStream
+  and FileOutputStream into a file and then read all the object from the
+  file using objectInputSteam and FileInputStream.
+ */
+public class ObjectInputOutputStreamExample {
+    public static void main(String[] args) throws IOException, ClassCastException, InterruptedException {
+
+        Integer number = 100;
+        String msg = "Hello Input/Output Stream";
+        Student student = new Student("Aarti", 100);
+
+        OutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Dell\\IdeaProjects\\java-io-concepts\\byte-streaming\\src\\object.txt");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+        objectOutputStream.writeObject(number);
+        objectOutputStream.writeObject(msg);
+        objectOutputStream.writeObject(student);
+
+        Thread.sleep(3000);
+
+        InputStream fileInputStream = new FileInputStream("C:\\Users\\Dell\\IdeaProjects\\java-io-concepts\\byte-streaming\\src\\object.txt");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        //this objectInputStream will read objects from specified fileInputStream
+
+        Integer integerObjectFromFile = (Integer) objectInputStream.readObject();
+        String stringObjectFromFile = (String) objectInputStream.readObject();
+        Student studentObjectFromFile = (Student) objectInputStream.readObject();
+        System.out.println("integerObjectFromFile : "+integerObjectFromFile);
+        System.out.println("stringObjectFromFile : "+stringObjectFromFile);
+        System.out.println("studentObjectFromFile : "+studentObjectFromFile);
+
+
+    }
+
+
