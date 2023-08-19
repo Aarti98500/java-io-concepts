bff1e85bdf131f4193f2634ba1377dc04be87e2d
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-08-19 22:21:58 +0530

    Buffered reader and writer example.

---
+++ character-streaming/src/BufferedReaderWriterExample.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class BufferedReaderWriterExample {
+    public static void main(String[] args) throws IOException {
+
+        try(FileReader fileReader = new FileReader("C:\\Users\\Dell\\IdeaProjects\\java-io-concepts\\character-streaming\\src\\input.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ){
+            char[] charsFromInputFile = new char[100];
+            bufferedReader.read(charsFromInputFile);
+            System.out.println("Input string read by buffer reader from input.txt" + Arrays.toString(charsFromInputFile));
+            try(FileWriter fileWriter = new FileWriter("C:\\Users\\Dell\\IdeaProjects\\java-io-concepts\\character-streaming\\src\\output.txt");
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                ) {
+
+
+            }
+        }
+
+    }
+}
